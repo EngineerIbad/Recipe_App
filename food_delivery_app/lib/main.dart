3a8da556b18b77f2e1cc2ee91418d89cbd656771
Ibad Mughal <ibadamir3@gmail.com>
2023-12-07 16:11:25 +0500

    Done Setting keys folder

--- food_delivery_app/lib/main.dart
+++ food_delivery_app/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:food_delivery_app/routes/route_names.dart';
 import 'package:food_delivery_app/routes/screen_routes.dart';
 import 'package:food_delivery_app/services/local_db/db_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// Screen Orientation is restricted to Portrait Mode Only
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
+
+  /// Local Database Is Initialized
   await DBHandler.initializeDB();
   runApp(const MyApp());
 }
@@ -17,7 +25,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(199, 176, 186, 36)),
         useMaterial3: true,
       ),
       initialRoute: RoutesName.splash,
--- food_delivery_app/lib/ui/screens/splash/splash_screen.dart
+++ food_delivery_app/lib/ui/screens/splash/splash_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  String key = "Nothing loaded";
+
+  loadKey() async {
+    DotEnv dotEnv = DotEnv();
+    await dotEnv.load(fileName: 'lib/keys/api_key.env');
+    key = dotEnv.get("API_KEY");
+    if(mounted) setState((){});
+  }
+
+  @override
+  void initState() {
+    loadKey();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(child: Text(key)),
+    );
   }
 }
